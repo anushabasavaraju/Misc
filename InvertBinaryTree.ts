23aa9b254df3ad0dbd281e74c013e8ba58b8fb6a
anushabasavaraju <33107353+anushabasavaraju@users.noreply.github.com>
2025-02-11 14:04:17 -0700

    Create InvertBinaryTree.ts

---
+++ InvertBinaryTree.ts
@@ -0,0 +1,50 @@
+/*
+ Given the root of a binary tree, invert the tree, and return its root.
+
+Example 1:
+Input: root = [4,2,7,1,3,6,9]
+Output: [4,7,2,9,6,3,1]
+
+Example 2:
+Input: root = [2,1,3]
+Output: [2,3,1]
+
+Example 3:
+Input: root = []
+Output: []*/
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+class TreeNode {
+      val: number
+      left: TreeNode | null
+      right: TreeNode | null
+      constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+          this.val = (val===undefined ? 0 : val)
+          this.left = (left===undefined ? null : left)
+          this.right = (right===undefined ? null : right)
+      }
+  }
+
+function invertTree(root: TreeNode | null) : TreeNode | null {
+  if(!root) return null;
+  const right = root.right;
+  root.right = invertTree(root.left);
+  root.left = invertTree(right);
+  return root;
+}
+
+const invertedTree = invertTree([4,2,7,1,3,6,9]);
+console.log('Test result: ' +invertedTree);
